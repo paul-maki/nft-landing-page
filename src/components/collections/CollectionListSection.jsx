aa7e19277210caaf1fc2056c3b61fc2b39f8b1de
Paul Maki <dynamicsdeveloper2@centauro.net>
2022-04-14 10:10:42 +0200

    Add style to collection dataç

--- src/components/collections/CollectionListSection.jsx
+++ src/components/collections/CollectionListSection.jsx
@@ -4,7 +4,10 @@ import { CollectionList } from './CollectionList';
 export const CollectionListSection = () => {
     return (
         <section className={styles.section}>
-            <h1>Las mejores colecciones sobre Los últimos 7 días</h1>
+            <h2 className={styles['section-title']}>
+                <span>Las mejores colecciones sobre</span>
+                <span className={styles['section-title-days']}> los últimos 7 días</span>
+            </h2>
             <CollectionList />
         </section>
     )
