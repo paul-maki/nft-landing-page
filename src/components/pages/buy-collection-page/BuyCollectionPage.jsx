ba73a69a5bc959f81751af79b3ec4b9d41d6bede
Paul Maki <dynamicsdeveloper2@centauro.net>
2022-04-19 16:48:14 +0200

    Add layout to buy collection

--- src/components/pages/buy-collection-page/BuyCollectionPage.jsx
+++ src/components/pages/buy-collection-page/BuyCollectionPage.jsx
@@ -4,15 +4,17 @@ import artistCoolCats from '../../../images/buy-collection/header/artist-cool-ca
 import headerCoolCats from '../../../images/buy-collection/header/header-cool-cats.png';
 import { BuyCollectionListContainer } from "../../buy-collection/buy-collection-list/BuyCollectionListContainer";
 import styles from './buy-collection-page.module.scss';
+import { WrapperLayout } from "../../layout/Wrapper-layout";
 
 export const BuyCollectionPage = () => {
-     return(
-         <div>
+     return(<>
             <BuyCollectionHeader artistImg={artistCoolCats} artistName="CoolCatsContract" headerImg={headerCoolCats} title="Cool Cats NFT"/>
-            <div className={styles['collection-container']}>
-                <BuyCollectionFilter />
-                <BuyCollectionListContainer />
-            </div>
-         </div>
+                <WrapperLayout>
+                    <div className={styles['collection-container']}>
+                        <BuyCollectionFilter />
+                        <BuyCollectionListContainer />
+                    </div>
+                </WrapperLayout>
+            </>
      )
 }
