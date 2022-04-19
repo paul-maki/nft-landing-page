ee10bdce827b28777a3a239375a0b7c39281cdad
Paul Maki <dynamicsdeveloper2@centauro.net>
2022-04-19 16:18:27 +0200

    Add responsive to buy collections

--- src/components/pages/buy-collection-page/BuyCollectionPage.jsx
+++ src/components/pages/buy-collection-page/BuyCollectionPage.jsx
@@ -3,13 +3,16 @@ import { BuyCollectionHeader } from "../../buy-collection/buy-collection-header/
 import artistCoolCats from '../../../images/buy-collection/header/artist-cool-cats.png';
 import headerCoolCats from '../../../images/buy-collection/header/header-cool-cats.png';
 import { BuyCollectionListContainer } from "../../buy-collection/buy-collection-list/BuyCollectionListContainer";
+import styles from './buy-collection-page.module.scss';
 
 export const BuyCollectionPage = () => {
      return(
          <div>
             <BuyCollectionHeader artistImg={artistCoolCats} artistName="CoolCatsContract" headerImg={headerCoolCats} title="Cool Cats NFT"/>
-            <BuyCollectionFilter />
-            <BuyCollectionListContainer />
+            <div className={styles['collection-container']}>
+                <BuyCollectionFilter />
+                <BuyCollectionListContainer />
+            </div>
          </div>
      )
 }
